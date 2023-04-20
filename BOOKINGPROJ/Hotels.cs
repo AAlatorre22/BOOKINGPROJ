baedbe5d448ff9c45377e6204b8e05ae1cb8768e
AAlatorre22 <126432462+AAlatorre22@users.noreply.github.com>
2023-04-20 10:51:54 -0700

    first ApiCall

---
+++ BOOKINGPROJ/Hotels.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOKINGPROJ
+{
+    internal class Hotels
+    {
+
+        public static void Info(string APIKey)
+        {
+        //    var client = new RestClient("https://booking-com.p.rapidapi.com/v1/hotels/data?hotel_id=1377073&locale=en-gb");
+        //    var request = new RestRequest();
+        //    request.AddHeader("X-RapidAPI-Key", $"{APIkey}");
+        //    request.AddHeader("X-RapidAPI-Host", "booking-com.p.rapidapi.com");
+        //    var response = client.Execute(request);
+        }
+
+    }
+}
--- BOOKINGPROJ/Program.cs
+++ BOOKINGPROJ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace BOOKINGPROJ
 {
@@ -9,8 +10,26 @@ namespace BOOKINGPROJ
 
             string key = File.ReadAllText("appsettings.json");
             string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
-            
 
+            //string apiCall = $"https://booking-com.p.rapidapi.com/v1/hotels/data?hotel_id=1377073&locale=en-gb";
+
+            //var client = new RestClient("https://booking-com.p.rapidapi.com/v1/hotels/data?hotel_id=1377073&locale=en-gb");
+            //var request = new RestRequest();
+            //request.AddHeader("X-RapidAPI-Key", $"{APIkey}");
+            //request.AddHeader("X-RapidAPI-Host", "booking-com.p.rapidapi.com");
+            //var response = client.Execute(request);
+
+            //Console.WriteLine(response);
+
+            string apiCall = $"https://booking-com.p.rapidapi.com/v1/hotels/data?hotel_id=1377073&locale=en-gb";
+
+            var client = new RestClient(apiCall);
+            var request = new RestRequest();
+            request.AddHeader("X-RapidAPI-Key", $"{APIkey}");
+            request.AddHeader("X-RapidAPI-Host", "booking-com.p.rapidapi.com");
+            var response = client.Get(request);
+            Console.WriteLine(response.Content.ToString());
+            Console.Read();
 
         }
     }
