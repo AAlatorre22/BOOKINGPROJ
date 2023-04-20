92fadb8aba7f7384b5276dc2eb25576758cc0825
AAlatorre22 <126432462+AAlatorre22@users.noreply.github.com>
2023-04-20 10:02:32 -0700

    Added json and api key

--- BOOKINGPROJ/Program.cs
+++ BOOKINGPROJ/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace BOOKINGPROJ
+﻿using Newtonsoft.Json.Linq;
+
+namespace BOOKINGPROJ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string key = File.ReadAllText("appsettings.json");
+            string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+            
+
+
         }
     }
 }
